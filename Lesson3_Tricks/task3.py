fa48c73577bca815f5880b2867461f514f70733d
Andrii Sidachenko <andrii.sidachenko@refinitiv.com>
2022-06-06 00:17:59 +0300

    hw #3 - task #3 - logging argparse

--- Lesson3_Tricks/task3.py
+++ Lesson3_Tricks/task3.py
@@ -7,13 +7,59 @@
 путь к пакету в INFO и версию пакета в DEBUG.
 •	уровень логирования задается отдельно для консоли и отдельно для файла через параметры командной строки,
 используя библиотеку argparse.
+
+---->
+example to check:
+1) python task3.py -type f -l info
+2) python task3.py -type c -l info
 """
-import sys
+
+import argparse
 import importlib.util
+import sys
+
 from loguru import logger
 
 
-logger.add("get_package_info_{time}.log", level="WARNING", rotation="10 MB")
+def parse_cmd_args():
+    type_help = "logging type - console or file"
+    level_help = "logging level - 'debug', 'info', 'warning', 'error' or 'critical'"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-type", nargs="?", default=None, choices=["c", "f"], help=type_help
+    )
+    parser.add_argument(
+        "-l",
+        "--level",
+        default="info",
+        choices=["debug", "info", "warning", "error", "critical"],
+        help=level_help,
+    )
+
+    if len(sys.argv) <= 2:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    cmd, _ = parser.parse_known_args()
+
+    console, file = True, True
+    if cmd.type == "c":
+        file = False
+    if cmd.type == "f":
+        console = False
+    return console, file, cmd.level.upper()
+
+
+def set_logging(console=True, file=True, level=None):
+    if console:
+        logger.add(
+            sys.stderr,
+            level=level,
+            format="<red>[{level}]</red> Message : <green>{message}</green> @ {time}",
+            colorize=True,
+        )
+    if file:
+        logger.add(sink="get_package_info_{time}.log", level=level, rotation="10 MB")
 
 
 def get_package_path(package_name):
@@ -29,5 +75,7 @@ def get_package_path(package_name):
 
 
 if __name__ == "__main__":
+    args = parse_cmd_args()
+    set_logging(*args)
     result = get_package_path("loguru")
     print(result)
